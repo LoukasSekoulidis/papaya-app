3f30bbf7be7b22cbd34efd5e9bf4021a79c7acb5
Stephan Hartmann <>
2022-05-18 23:54:41 +0200

    couldn't start backend, had to change path

--- backend/endpoints/note/NoteRoute.js
+++ backend/endpoints/note/NoteRoute.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
-var AuthenticationService = require('..authentication/AuthenticationService')
+// var AuthenticationService = require('..authentication/AuthenticationService')
+var AuthenticationService = require('../authentication/AuthenticationService')
 var NoteService = require('./NoteService')
 
 // Notiz neu anlegen
