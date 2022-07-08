dba1c05c21b4994a11475bac751fdc517f65fca2
Lukas Sekoulidis <sekoulidis.lukas@icloud.com>
2022-07-08 11:57:16 +0200

    Changed API to find User by ID, starting new confirmation process if userMail is changed

--- backend/endpoints/user/UserRoute.js
+++ backend/endpoints/user/UserRoute.js
@@ -5,59 +5,60 @@ const userService = require('./UserService')
 const authenticationService = require('../authentication/AuthenticationService')
 
 router.get('/', authenticationService.isAuthenticated, (req, res, next) => {
-  userService.getUsers((err, users) => {
-    if (users) {
-      res.status(200).json(users);
-    }
-    else {
-      res.status(404).json({ Error: err });
-    }
-  });
+    userService.getUsers((err, users) => {
+        if (users) {
+            res.status(200).json(users);
+        }
+        else {
+            res.status(404).json({ Error: err });
+        }
+    });
 });
 
-router.get('/:userName', authenticationService.isAuthenticated, (req, res, next) => {
-  userService.getUser(userName, (err, user) => {
-    if (user) {
-      res.status(200).json(user);
-    }
-    else {
-      res.status(404).json({ Error: err });
-    }
-  });
+router.get('/:userID', authenticationService.isAuthenticated, (req, res, next) => {
+
+    userService.getUser(req.url.split('/')[1], (err, user) => {
+        if (user) {
+            res.status(200).json(user);
+        }
+        else {
+            res.status(404).json({ Error: err });
+        }
+    });
 });
 
 router.post('/create', (req, res, next) => {
-  console.log('In Post!')
-  userService.createUser(req.body, (err, postedUser) => {
-    if (postedUser) {
-      res.status(201).json(postedUser);
-    }
-    else {
-      res.status(404).json({ Error: err });
-    }
-  });
+    console.log('In Post!')
+    userService.createUser(req.body, (err, postedUser) => {
+        if (postedUser) {
+            res.status(201).json(postedUser);
+        }
+        else {
+            res.status(404).json({ Error: err });
+        }
+    });
 });
 
-router.put('/update/:userName', authenticationService.isAuthenticated, (req, res, next) => {
-  userService.updateUser(req.url.split('/')[2], req.body, (err, updatedUser) => {
-    if (updatedUser) {
-      res.status(200).json(updatedUser);
-    }
-    else {
-      res.status(404).json({ Error: err });
-    }
-  });
+router.put('/update/:userID', authenticationService.isAuthenticated, (req, res, next) => {
+    userService.updateUser(req.url.split('/')[2], req.body, (err, updatedUser) => {
+        if (updatedUser) {
+            res.status(200).json(updatedUser);
+        }
+        else {
+            res.status(404).json({ Error: err });
+        }
+    });
 });
 
-router.delete('/delete/:userName', authenticationService.isAuthenticated, (req, res, next) => {
-  userService.deleteUser(req.url.split('/')[2], (err, deletedUser) => {
-    if (deletedUser) {
-      res.status(200).json({ Deleted: deletedUser });
-    }
-    else {
-      res.status(404).json({ Error: err });
-    }
-  });
+router.delete('/delete/:userID', authenticationService.isAuthenticated, (req, res, next) => {
+    userService.deleteUser(req.url.split('/')[2], (err, deletedUser) => {
+        if (deletedUser) {
+            res.status(200).json({ Deleted: deletedUser });
+        }
+        else {
+            res.status(404).json({ Error: err });
+        }
+    });
 });
 
 module.exports = router;
--- backend/endpoints/user/UserService.js
+++ backend/endpoints/user/UserService.js
@@ -6,102 +6,116 @@ const userModel = require('./UserModel');
 const nodeMailer = require('../confirmation/ConfirmationMailer');
 
 function getUsers(callback) {
-  userModel.find((err, users) => {
-    if (err) {
-      return callback(err, null);
-    }
-    else {
-      return callback(null, users);
-    }
-  });
+    userModel.find((err, users) => {
+        if (err) {
+            return callback(err, null);
+        }
+        else {
+            return callback(null, users);
+        }
+    });
 };
 
-function getUser(userName, callback) {
-  userModel.findOne({ userName: userName }, (err, user) => {
-    if (err) {
-      return callback(err, null);
-    }
-    else if (!user) {
-      return callback(`No User -- ${userName} -- in Database`, null);
-    }
-    else {
-      return callback(null, user);
-    }
-  });
+function getUser(userID, callback) {
+    userModel.findById(userID, (err, user) => {
+        if (err) {
+            return callback(err, null);
+        }
+        else if (!user) {
+            return callback(`No User -- ${userName} -- in Database`, null);
+        }
+        else {
+            return callback(null, user);
+        }
+    });
 };
 
 function createUser(props, callback) {
-  const token = jwt.sign({userMail: props.userMail}, config.get('session.tokenKey'));
+    const token = jwt.sign({ userMail: props.userMail }, config.get('session.tokenKey'));
 
-  const newUser = new User({
-    userMail: props.userMail,
-    userName: props.userName,
-    password: props.password,
-    isAdministrator: props.isAdministrator,
-    confirmationCode: token
-  });
-  newUser.save((err, user) => {
-    if (err && err.code === 11000) {
-      return callback('Duplicate Key Error: User with given userMail already exists!', null);
-    }
-    else if (err && err.name === 'ValidationError') {
-      return callback('Validation Error: Required attribute Missing: ' + err, null);
-    }
-    else if (err) {
-      return callback(err, null);
-    }
-    else {
-      nodeMailer.sendConfirmationMail(
-        user.userName,
-        user.userMail,
-        user.confirmationCode
-      )
-      return callback(null, user);
-    }
-  });
-};
-
-function updateUser(userName, props, callback) {
-  userModel.findOne({ userName: userName }, function (err, user) {
-    if (err) {
-      return callback(err)
-    }
-    else if (!user) {
-      return callback(`Problem finding User: User with given userMail -- ${userName} -- does not exist!`);
-    }
-    else {
-      Object.assign(user, props);
-      user.save((err) => {
-        if (err) {
-          return callback('Problem saving User: ' + err, null);
+    const newUser = new User({
+        userMail: props.userMail,
+        userName: props.userName,
+        password: props.password,
+        isAdministrator: props.isAdministrator,
+        confirmationCode: token
+    });
+    newUser.save((err, user) => {
+        if (err && err.code === 11000) {
+            return callback('Duplicate Key Error: User with given userMail already exists!', null);
+        }
+        else if (err && err.name === 'ValidationError') {
+            return callback('Validation Error: Required attribute Missing: ' + err, null);
+        }
+        else if (err) {
+            return callback(err, null);
         }
         else {
-          return callback(null, user);
+            nodeMailer.sendConfirmationMail(
+                user.userName,
+                user.userMail,
+                user.confirmationCode
+            )
+            return callback(null, user);
         }
-      })
-    }
-  });
+    });
+};
+
+function updateUser(userID, props, callback) {
+    userModel.findById(userID, function (err, user) {
+        if (err) {
+            return callback(err)
+        }
+        else if (!user) {
+            return callback(`Problem finding User: User with given userMail -- ${userName} -- does not exist!`);
+        }
+        else {
+            if (props.userMail !== undefined) {
+                const token = jwt.sign({ userMail: props.userMail }, config.get('session.tokenKey'));
+                props.confirmed = false;
+                props.confirmationCode = token;
+
+                Object.assign(user, props);
+
+                nodeMailer.sendConfirmationMail(
+                    user.userName,
+                    props.userMail,
+                    token
+                )
+            } else {
+                Object.assign(user, props);
+            }
+            user.save((err) => {
+                if (err) {
+                    return callback('Problem saving User: ' + err, null);
+                }
+                else {
+                    return callback(null, user);
+                }
+            })
+        }
+    });
 }
 
-function deleteUser(userName, callback) {
-  userModel.findOneAndDelete({ userName: userName }, function (err, deletetUser) {
-    if (err) {
-      return callback(err);
-    }
-    else if (!deletetUser) {
-      return callback('Error deleting User: No user in database with given userName: ' + userName);
-    }
-    else {
-      return callback(null, userName);
-    }
-  })
+function deleteUser(userID, callback) {
+    userModel.findByIdAndDelete(userID, function (err, deletetUser) {
+        if (err) {
+            return callback(err);
+        }
+        else if (!deletetUser) {
+            return callback('Error deleting User: No user in database with given userName: ' + userName);
+        }
+        else {
+            return callback(null, userName);
+        }
+    })
 }
 
 module.exports = {
-  getUsers,
-  getUser,
-  createUser,
-  updateUser,
-  deleteUser,
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
 };
 
