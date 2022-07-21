9480b022ccf49a765116cb05fa60d8b19748cd03
Lukas Sekoulidis <sekoulidis.lukas@icloud.com>
2022-07-21 14:54:59 +0200

    https server to http

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const https = require('https');
-const fs = require('fs');
-
 const database = require('./database/db');
 const userRoutes = require('./endpoints/user/UserRoute');
 const noteRoutes = require('./endpoints/note/NoteRoute');
@@ -42,23 +39,14 @@ database.initDB((error, db) => {
 
 /* Error Handler: */
 app.use((req, res, next) => {
-    // res.status(404).send('Can not find that! The url is not supported!');
-    res.status(404).send(req.originalUrl);
+    res.status(404).send('Can not find that! The url is not supported!');
 });
 
 app.use((err, req, res, next) => {
     res.status(500).send("Something broke: " + err.stack);
 });
 
-const port = 443;
-
-https
-    .createServer(
-        {
-            key: fs.readFileSync("./certificates/key.pem"),
-            cert: fs.readFileSync("./certificates/cert.pem"),
-        },
-        app)
-    .listen(port, () => {
-        console.log(`App listening at https://localhost:${port}`);
-    })
+const port = 8080;
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+})
