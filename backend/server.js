59c8a1004f88b126287d5f1811b4ac88d311c36b
Lukas Sekoulidis <sekoulidis.lukas@icloud.com>
2022-07-21 14:29:27 +0200

    http server to https

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const https = require('https');
+const fs = require('fs');
+
 const database = require('./database/db');
 const userRoutes = require('./endpoints/user/UserRoute');
 const noteRoutes = require('./endpoints/note/NoteRoute');
@@ -14,8 +17,8 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-  origin: '*',
-  exposedHeaders: ['Authorization']
+    origin: '*',
+    exposedHeaders: ['Authorization']
 }));
 
 app.use(bodyParser.json());
@@ -28,26 +31,34 @@ app.use('/api/v1/category', categoryRoutes);
 
 
 database.initDB((error, db) => {
-  if (db) {
-    console.log('Database succesfully binded');
+    if (db) {
+        console.log('Database succesfully binded');
 
-  }
-  else {
-    console.log('Database binding not succesfull');
-  }
+    }
+    else {
+        console.log('Database binding not succesfull');
+    }
 });
 
 /* Error Handler: */
 app.use((req, res, next) => {
-  // res.status(404).send('Can not find that! The url is not supported!');
-  res.status(404).send(req.originalUrl);
+    // res.status(404).send('Can not find that! The url is not supported!');
+    res.status(404).send(req.originalUrl);
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send("Something broke: " + err.stack);
+    res.status(500).send("Something broke: " + err.stack);
 });
 
-const port = 8080;
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-})
+const port = 443;
+
+https
+    .createServer(
+        {
+            key: fs.readFileSync("./certificates/key.pem"),
+            cert: fs.readFileSync("./certificates/cert.pem"),
+        },
+        app)
+    .listen(port, () => {
+        console.log(`App listening at https://localhost:${port}`);
+    })
